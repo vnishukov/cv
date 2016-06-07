e0f2ac60e8083c8d9a1b3c73ed31c7aaf7240bb0
Vitaliy Nishukov <vnishukov@gmail.com>
2016-06-07 18:44:02 +0300

    Segments links

--- js/cv.js
+++ js/cv.js
@@ -2,37 +2,51 @@ $(document).ready(function () {
   var data = [
     {
       label: ['Optical Character Recognition', 'License Plate Recognition'],
+      link: 'http://google.com/',
       data: 1
     },
     {
       label: ['Optical flow', 'Driver Assistance Systems', 'Object Tracking'],
+      link: 'http://yahoo.com/',
       data: 1
     },
     {
       label: ['Object Detection', 'Object Recognition'],
+      link: 'http://amazon.com/',
       data: 1
     },
     {
       label: ['Face Position Estimation', 'Gesture Recognition', 'Face Recognition'],
+      link: 'http://oracle.com/',
       data: 1
     },
     {
       label: ['HeatMap', 'Face Detection'],
+      link: 'http://yelp.com/',
       data: 1
     },
     {
       label: ['Motion Detection', 'Image Processing', 'Counting'],
+      link: 'http://tripadvisor.com/',
       data: 1
     },
     {
       label: ['Inttrusion Area'],
+      link: 'http://github.com/',
       data: 1
     },
     {
       label: ['Fire Detection'],
+      link: 'http://ebay.com/',
       data: 1
     }
   ];
+
+  $('.pie-chart').bind('plotclick', function (event, pos, item) {
+    if (item) {
+      document.location.href = item.series.link;
+    }
+  });
 
   $.plot('.pie-chart', data, {
     series: {
@@ -56,14 +70,14 @@ $(document).ready(function () {
     }
   });
 
-  function labelFormatter(label) {
+  function labelFormatter(label, series) {
     var result = '';
     _.each(label, function (entry) {
-      result += '<p><strong>' + entry + '</strong></p>'
+      result += '<a class="chart-label" href="' + series.link + '">' + entry + '</a>'
     });
     return result;
   }
 
-  $('#pieLabel0').css('left', 585);
-  $('#pieLabel1').css('top', 240);
+  $('#pieLabel0').css('left', 580);
+  $('#pieLabel1').css('top', 245);
 });
